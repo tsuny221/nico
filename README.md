c0914272b08f2a8ede5cd92b74852f40214130f7
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-20 17:21:18 +0900

    [Star] nico

---
+++ README.md
@@ -0,0 +1,3 @@
+# オリジナルサイト制作
+## 『*nico*』
+【使用言語】HTML, CSS, jQuery etc...
---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".slider").slick({
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    speed: 1500,
+    fade: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
